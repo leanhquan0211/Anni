b1e964e9e331b0f032709c2fc0f3a7dc67abc14c
lekuan <anhquan02114869@gmail.com>
2025-09-08 00:19:50 +0700

    Cập nhật code Anni, cấu hình Vite

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: mode === "production" ? "/Anni/" : "/",  // 👈 dev thì base = "/", build thì /Anni/
   server: {
     host: "::",
     port: 8080,
